da2ca68e639d2b6de5af84f795a60b049b8b2fb7
Clement <cdreptin@gmail.com>
2020-02-11 13:56:34 +0100

    QR code reader ok

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ import Home from "./components/Home";
 
 Vue.prototype.$bus = new Vue();
 
+Vue.registerElement('BarcodeScanner', () => require('nativescript-barcodescanner').BarcodeScannerView);
+
 new Vue({
 
     template: `
